dc94db78d5b0f270ac0a0f52c0471266c8403073
Tonyopano <antonyopano@gmail.com>
2024-03-01 17:20:57 +0300

    Accordian single and multiple selection

---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,61 @@
+/*single selection*/
+//multiple selection
+
+import { useState } from "react";
+import data from "./data";
+import "./style.css";
+
+const Accordian = () => {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiselection, setEnableMultiselection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  const handleSingleSelection = (getCurrentId) => {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  const handleMultipleSelection = (getCurrentId) => {
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+
+    setMultiple(cpyMultiple);
+  }
+
+  console.log(selected, multiple);
+  return (
+    <div className="wrapper">
+      <button onClick={() => setEnableMultiselection(!enableMultiselection)}>
+        Enable Multiple Selection
+      </button>
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={
+                  enableMultiselection
+                    ? () => handleMultipleSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id || multiple.indexOf(dataItem.id) !== -1 ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No dta found !</div>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Accordian;
