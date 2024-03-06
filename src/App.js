2a5b7b04888d97594a39f80f5aae303741ffd770
Tonyopano <antonyopano@gmail.com>
2024-03-06 10:00:00 +0300

    Defining component logic for fetching and loading data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from "react";
 import "./App.css";
 import Accordian from "./components/accordian";
-import RandomColor from "./components/random-color_generator"
+import RandomColor from "./components/random-color_generator";
 import StarRating from "./components/star-rating";
-
+import ImageSlider from "./components/image-slider";
 
 const App = () => {
   return (
     <div className="App">
       {/*Accordion component*/}
-      {/* <Accordian /> */} 
+      {/* <Accordian /> */}
 
       {/* {Random color generator component} */}
       {/* <RandomColor /> */}
 
       {/* {Star rating component} */}
-      {<StarRating noOfStars={10} />}
+      {/* {<StarRating noOfStars={10} />} */}
 
-
+      {/* {Image slider component} */}
+      {
+        <ImageSlider
+          url={"https://picsum.photos/v2/list"}
+          page={"1"}
+          limit={"10"}
+        />
+      }
     </div>
   );
 };
-
 export default App;
--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
 
-const ImageSlider = () => {
-  return (
-    <div>ImageSlider</div>
-  )
-}
+const ImageSlider = ({ url, limit = 5, page = 1}) => {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-export default ImageSlider
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
+
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
+
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMsg(e.message);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
+
+  console.log(images);
+
+  if (loading) {
+    return <div>Loading data please wait</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div>Error occured ! {errorMsg}</div>;
+  }
+
+  return <div className="container">ImageSlider</div>;
+};  
+
+export default ImageSlider;
