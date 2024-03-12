66f95f14f85eccf2a432c0c69e7754e595bc13f2
Tonyopano <antonyopano@gmail.com>
2024-03-12 22:04:02 +0300

    Adding data and menu components

---
+++ src/components/tree-view/menu-list.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuItem from './menu-item'
+
+const MenuList = ({list = []}) => {
+  return <div className='menu-list-container'>
+    {
+        list && list.length ? 
+        list.map((listItem) => <MenuItem item={listItem} />)
+        : null}
+  </div>
+  );
+}
+
+export default MenuList
